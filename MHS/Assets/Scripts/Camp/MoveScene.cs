af4c7b8ed80c99fb5c774b8b244175c989abfcbf
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2023-09-05 18:40:58 +0900

    📌 데이터 연구소 -> 캠프 버튼 추가

--- MHS/Assets/Scripts/Camp/MoveScene.cs
+++ MHS/Assets/Scripts/Camp/MoveScene.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MoveScene : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -35,5 +36,10 @@ public class MoveScene : MonoBehaviour
     public void MoveShipScene()
     {
         SceneManager.LoadScene("Ship");
+    }
+
+    public void MoveCampScene()
+    {
+        SceneManager.LoadScene("Camp");
     }
 }
