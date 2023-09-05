05286495d8044fbc3c456c851e2ee791189cd054
ksb3458 <86918962+ksb3458@users.noreply.github.com>
2023-09-05 19:12:37 +0900

    CreateItem.cs 수정

--- MHS/Assets/Scripts/CreateItem.cs
+++ MHS/Assets/Scripts/CreateItem.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public struct Delivery
 {
     public string country;
     public string data;
+    public int count;
 }
 
 public class CreateItem : MonoBehaviour
@@ -26,10 +28,21 @@ public class CreateItem : MonoBehaviour
         "포항 과메기", "순천 딸기", "장흥 매생이", "제주 한라봉"
     };
 
+    public Text[] orderText;
+
     // Start is called before the first frame update
     void Start()
     {
-        List<Delivery> selectedData = GetRandomSelection(3);
+        string data = PlayerPrefs.GetString("orderList");
+        string[] loadedOrderList = LoadStringArray("orderList");
+        List<Delivery> createData = GetRandomSelection(5 - loadedOrderList.Length);
+        SaveStringArray("orderList", loadedOrderList, createData);
+
+        string[] loadedOrderList2 = LoadStringArray("orderList");
+        for(int i=0; i<orderText.Length; i++)
+        {
+            orderText[i].text = loadedOrderList2[i + 1];
+        }
     }
 
     List<Delivery> GetRandomSelection(int count)
@@ -42,12 +55,39 @@ public class CreateItem : MonoBehaviour
             Delivery delivery = new Delivery();
             int randomCountryIndex = Random.Range(0, countryList.Count);
             int randomDataIndex = Random.Range(0, tempList.Count);
+            int randomCount = Random.Range(0, 19);
             delivery.country = countryList[randomCountryIndex];
             delivery.data = tempList[randomDataIndex];
+            delivery.count = randomCount * 10 + 20;
             tempList.RemoveAt(randomDataIndex);
             result.Add(delivery);
         }
         return result;
+    }
+
+    string[] LoadStringArray(string key)
+    {
+        if (PlayerPrefs.HasKey(key))
+        {
+            string data = PlayerPrefs.GetString(key);
+            return data.Split(',');
+        }
+        else
+        {
+            return new string[0];
+        }
+    }
+
+    void SaveStringArray(string key, string[] loadedOrderList, List<Delivery> createData)
+    {
+        string data = string.Join(",", loadedOrderList);
+        for(int i=0; i<createData.Count; i++)
+        {
+            string newData = ",[" + createData[i].country + "]" + createData[i].data + " " + createData[i].count + "개";
+            data += newData;
+        }
+        PlayerPrefs.SetString(key, data);
+        PlayerPrefs.Save();
     }
 
     // Update is called once per frame
---
+++ MHS/Assets/Scripts/UniteData.cs
@@ -0,0 +1,8 @@
+#define RELEASE_D
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniteData
+{
+    public static string orderList;
+}
